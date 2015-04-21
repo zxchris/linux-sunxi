bd28d11a36867ce9f87c32f85f8d96c2dee6aeba
dan-and <daniel@danand.de>
2015-04-21 17:03:02 +0200

    finally removed the experimental MALI R4P0 code again.

--- drivers/gpu/mali/mali/common/mali_osk_profiling.h
+++ drivers/gpu/mali/mali/common/mali_osk_profiling.h
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2010-2014 ARM Limited. All rights reserved.
- * 
+ * Copyright (C) 2010-2012 ARM Limited. All rights reserved.
+ *
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
- * 
+ *
  * A copy of the licence is included with the program, and can also be obtained from Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
@@ -11,11 +11,13 @@
 #ifndef __MALI_OSK_PROFILING_H__
 #define __MALI_OSK_PROFILING_H__
 
-#if defined(CONFIG_MALI400_PROFILING) && defined (CONFIG_TRACEPOINTS)
+#if MALI_TIMELINE_PROFILING_ENABLED
 
+#if defined (CONFIG_TRACEPOINTS) && !MALI_INTERNAL_TIMELINE_PROFILING_ENABLED
 #include "mali_linux_trace.h"
+#endif /* CONFIG_TRACEPOINTS && !MALI_INTERNAL_TIMELINE_PROFILING_ENABLED */
+
 #include "mali_profiling_events.h"
-#include "mali_profiling_gator_api.h"
 
 #define MALI_PROFILING_MAX_BUFFER_ENTRIES 1048576
 
@@ -44,7 +46,7 @@ void _mali_osk_profiling_term(void);
  * @param limit The desired maximum number of events to record on input, the actual maximum on output.
  * @return _MALI_OSK_ERR_OK on success, otherwise failure.
  */
-_mali_osk_errcode_t _mali_osk_profiling_start(u32 *limit);
+_mali_osk_errcode_t _mali_osk_profiling_start(u32 * limit);
 
 /**
  * Add an profiling event
@@ -57,8 +59,13 @@ _mali_osk_errcode_t _mali_osk_profiling_start(u32 *limit);
  * @param data4 Fifth data parameter, depending on event_id specified.
  * @return _MALI_OSK_ERR_OK on success, otherwise failure.
  */
+#if defined (CONFIG_TRACEPOINTS) && !MALI_INTERNAL_TIMELINE_PROFILING_ENABLED
 /* Call Linux tracepoint directly */
 #define _mali_osk_profiling_add_event(event_id, data0, data1, data2, data3, data4) trace_mali_timeline_event((event_id), (data0), (data1), (data2), (data3), (data4))
+#else
+/* Internal profiling is handled like a plain function call */
+void _mali_osk_profiling_add_event(u32 event_id, u32 data0, u32 data1, u32 data2, u32 data3, u32 data4);
+#endif
 
 /**
  * Report a hardware counter event.
@@ -67,8 +74,13 @@ _mali_osk_errcode_t _mali_osk_profiling_start(u32 *limit);
  * @param value The value of the counter.
  */
 
+#if defined (CONFIG_TRACEPOINTS) && !MALI_INTERNAL_TIMELINE_PROFILING_ENABLED
 /* Call Linux tracepoint directly */
 #define _mali_osk_profiling_report_hw_counter(counter_id, value) trace_mali_hw_counter(counter_id, value)
+#else
+/* Internal profiling is handled like a plain function call */
+void _mali_osk_profiling_report_hw_counter(u32 counter_id, u32 value);
+#endif
 
 /**
  * Report SW counters
@@ -83,7 +95,7 @@ void _mali_osk_profiling_report_sw_counters(u32 *counters);
  * @param count Returns the number of recorded events.
  * @return _MALI_OSK_ERR_OK on success, otherwise failure.
  */
-_mali_osk_errcode_t _mali_osk_profiling_stop(u32 *count);
+_mali_osk_errcode_t _mali_osk_profiling_stop(u32 * count);
 
 /**
  * Retrieves the number of events that can be retrieved
@@ -101,7 +113,7 @@ u32 _mali_osk_profiling_get_count(void);
  * @param data The 5 data values for the retrieved event will be stored here.
  * @return _MALI_OSK_ERR_OK on success, otherwise failure.
  */
-_mali_osk_errcode_t _mali_osk_profiling_get_event(u32 index, u64 *timestamp, u32 *event_id, u32 data[5]);
+_mali_osk_errcode_t _mali_osk_profiling_get_event(u32 index, u64* timestamp, u32* event_id, u32 data[5]);
 
 /**
  * Clear the recorded buffer.
@@ -128,14 +140,6 @@ mali_bool _mali_osk_profiling_have_recording(void);
 
 /** @} */ /* end group _mali_osk_profiling */
 
-#else /* defined(CONFIG_MALI400_PROFILING)  && defined(CONFIG_TRACEPOINTS) */
-
-/* Dummy add_event, for when profiling is disabled. */
-
-#define _mali_osk_profiling_add_event(event_id, data0, data1, data2, data3, data4)
-
-#endif /* defined(CONFIG_MALI400_PROFILING)  && defined(CONFIG_TRACEPOINTS) */
+#endif /* MALI_TIMELINE_PROFILING_ENABLED */
 
 #endif /* __MALI_OSK_PROFILING_H__ */
-
-
