948d8e32e40b02425fb1c6ca821e1b20fa3587dc
Chris Smith <chris@zxdesign.info>
2017-08-17 20:03:36 +0100

    Script which pulls machid and compatibilty fixes from linux-sunxi and applies.

---
+++ arch/arm/fix_machid.sh
@@ -0,0 +1,14 @@
+wget https://github.com/linux-sunxi/linux-sunxi/commit/5052b83aa44dc16d6662d8d9d936166c139ad8c5.patch
+wget https://github.com/linux-sunxi/linux-sunxi/commit/9a1cd034181af628d4145202289e1993c1687db6.patch
+wget https://github.com/linux-sunxi/linux-sunxi/commit/c4c4664ed1a2f35e54a33ae4e65f517721ff43b5.patch
+wget https://github.com/linux-sunxi/linux-sunxi/commit/ade08aa6e5249a9e75a97393e86c250b2bcb3ec8.patch
+wget https://github.com/linux-sunxi/linux-sunxi/commit/16b25a95327f45a995f6efcf3e9d83a414231af9.patch
+wget https://github.com/linux-sunxi/linux-sunxi/commit/dea62f21deb177053b84b15a519dff6c74d061d9.patch
+wget https://github.com/linux-sunxi/linux-sunxi/commit/d47d367036be38c5180632ec8a3ad169a4593a88.patch
+git am 5052b83aa44dc16d6662d8d9d936166c139ad8c5.patch
+git am 9a1cd034181af628d4145202289e1993c1687db6.patch
+git am c4c4664ed1a2f35e54a33ae4e65f517721ff43b5.patch
+git am ade08aa6e5249a9e75a97393e86c250b2bcb3ec8.patch
+git am 16b25a95327f45a995f6efcf3e9d83a414231af9.patch
+git am dea62f21deb177053b84b15a519dff6c74d061d9.patch
+git am d47d367036be38c5180632ec8a3ad169a4593a88.patch
