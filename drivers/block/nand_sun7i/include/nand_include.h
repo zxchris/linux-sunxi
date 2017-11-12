f968a20b77008e25a0d307e25d8888cb4cdae504
Chris Smith <chris@zxdesign.info>
2017-11-12 19:55:14 +0000

    Further nand.

--- drivers/block/nand_sun7i/include/nand_include.h
+++ drivers/block/nand_sun7i/include/nand_include.h
@@ -1,11 +1,3 @@
-/*
- * Copyright (C) 2013 Allwinnertech, kevin.z.m <kevin@allwinnertech.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- */
-
 #ifndef __NAND_INCLUDE_H__
 #define __NAND_INCLUDE_H__
 
